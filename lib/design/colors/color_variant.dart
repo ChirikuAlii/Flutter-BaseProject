32dca348bb83d52292a079d54cff6e391cc8a3e5
Ali Rahmat Ismail <chiriku.ali.rhmat@gmail.com>
2025-10-14 19:26:42 +0700

    Adds color variant and design tokens
    Introduces a color variant class to manage light and dark
    mode colors.
    
    Adds a color token JSON file for managing theme colors and themes.
    
    This is a work in progress to integrate color methods
    with the overall themeing system.

---
+++ lib/design/colors/color_variant.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class CRColorVariant {
+  final Color light;
+  final Color? dark;
+
+  const CRColorVariant({required this.light, this.dark});
+
+  //default light
+
+  //if dark empty then dark mode same as light
+}
---
+++ lib/design/colors/colors2.dart
@@ -0,0 +1,165 @@
+import 'dart:ui';
+import 'package:flutter_base_project/design/colors/color_variant.dart';
+
+class CRColors2 {
+  final primary900 =
+      const CRColorVariant(light: Color(0xff1A998E), dark: Color(0xff1A998E));
+  final primary800 =
+      const CRColorVariant(light: Color(0xff31A399), dark: Color(0xff31A399));
+  final primary700 =
+      const CRColorVariant(light: Color(0xff48ADA5), dark: Color(0xff48ADA5));
+  final primary600 =
+      const CRColorVariant(light: Color(0xff5FB8B0), dark: Color(0xff5FB8B0));
+  final primary500 =
+      const CRColorVariant(light: Color(0xff76C2BB), dark: Color(0xff76C2BB));
+  final primary400 =
+      const CRColorVariant(light: Color(0xff8DCCC7), dark: Color(0xff8DCCC7));
+  final primary300 =
+      const CRColorVariant(light: Color(0xffA3D6D2), dark: Color(0xffA3D6D2));
+  final primary200 =
+      const CRColorVariant(light: Color(0xffBAE0DD), dark: Color(0xffBAE0DD));
+  final primary100 =
+      const CRColorVariant(light: Color(0xffD1EBE8), dark: Color(0xffD1EBE8));
+  final primary50 =
+      const CRColorVariant(light: Color(0xffE8F5F4), dark: Color(0xffE8F5F4));
+  final secondary900 =
+      const CRColorVariant(light: Color(0xff176B87), dark: Color(0xff176B87));
+  final secondary800 =
+      const CRColorVariant(light: Color(0xff2E7A93), dark: Color(0xff2E7A93));
+  final secondary700 =
+      const CRColorVariant(light: Color(0xff45899F), dark: Color(0xff45899F));
+  final secondary600 =
+      const CRColorVariant(light: Color(0xff5D97AB), dark: Color(0xff5D97AB));
+  final secondary500 =
+      const CRColorVariant(light: Color(0xff74A6B7), dark: Color(0xff74A6B7));
+  final secondary400 =
+      const CRColorVariant(light: Color(0xff8BB5C3), dark: Color(0xff8BB5C3));
+  final secondary300 =
+      const CRColorVariant(light: Color(0xffA2C4CF), dark: Color(0xffA2C4CF));
+  final secondary200 =
+      const CRColorVariant(light: Color(0xffB9D3DB), dark: Color(0xffB9D3DB));
+  final secondary100 =
+      const CRColorVariant(light: Color(0xffD1E1E7), dark: Color(0xffD1E1E7));
+  final secondary50 =
+      const CRColorVariant(light: Color(0xffE8F0F3), dark: Color(0xffE8F0F3));
+  final alertsStatusSuccess =
+      const CRColorVariant(light: Color(0xff12D18E), dark: Color(0xff12D18E));
+  final alertsStatusInfo =
+      const CRColorVariant(light: Color(0xff1A998E), dark: Color(0xff1A998E));
+  final alertsStatusWarning =
+      const CRColorVariant(light: Color(0xffFACC15), dark: Color(0xffFACC15));
+  final alertsStatusError =
+      const CRColorVariant(light: Color(0xffF75555), dark: Color(0xffF75555));
+  final alertsStatusLightDisabled =
+      const CRColorVariant(light: Color(0xffD8D8D8), dark: Color(0xffD8D8D8));
+  final alertsStatusDarkDisabled =
+      const CRColorVariant(light: Color(0xff23252B), dark: Color(0xff23252B));
+  final alertsStatusButtonDisabled =
+      const CRColorVariant(light: Color(0xff157A72), dark: Color(0xff157A72));
+  final greyscale900 =
+      const CRColorVariant(light: Color(0xff212121), dark: Color(0xff212121));
+  final greyscale800 =
+      const CRColorVariant(light: Color(0xff424242), dark: Color(0xff424242));
+  final greyscale700 =
+      const CRColorVariant(light: Color(0xff616161), dark: Color(0xff616161));
+  final greyscale600 =
+      const CRColorVariant(light: Color(0xff757575), dark: Color(0xff757575));
+  final greyscale500 =
+      const CRColorVariant(light: Color(0xff9E9E9E), dark: Color(0xff9E9E9E));
+  final greyscale400 =
+      const CRColorVariant(light: Color(0xffBDBDBD), dark: Color(0xffBDBDBD));
+  final greyscale300 =
+      const CRColorVariant(light: Color(0xffE0E0E0), dark: Color(0xffE0E0E0));
+  final greyscale200 =
+      const CRColorVariant(light: Color(0xffEEEEEE), dark: Color(0xffEEEEEE));
+  final greyscale100 =
+      const CRColorVariant(light: Color(0xffF5F5F5), dark: Color(0xffF5F5F5));
+  final greyscale50 =
+      const CRColorVariant(light: Color(0xffFAFAFA), dark: Color(0xffFAFAFA));
+  final othersWhite =
+      const CRColorVariant(light: Color(0xffFFFFFF), dark: Color(0xffFFFFFF));
+  final othersBlack =
+      const CRColorVariant(light: Color(0xff000000), dark: Color(0xff000000));
+  final othersRed =
+      const CRColorVariant(light: Color(0xffF54336), dark: Color(0xffF54336));
+  final othersPink =
+      const CRColorVariant(light: Color(0xffEA1E61), dark: Color(0xffEA1E61));
+  final othersPurple =
+      const CRColorVariant(light: Color(0xff9D28AC), dark: Color(0xff9D28AC));
+  final othersDeepPurple =
+      const CRColorVariant(light: Color(0xff673AB3), dark: Color(0xff673AB3));
+  final othersIndigo =
+      const CRColorVariant(light: Color(0xff3F51B2), dark: Color(0xff3F51B2));
+  final othersBlue =
+      const CRColorVariant(light: Color(0xff1A96F0), dark: Color(0xff1A96F0));
+  final othersLightBlue =
+      const CRColorVariant(light: Color(0xff00A9F1), dark: Color(0xff00A9F1));
+  final othersCyan =
+      const CRColorVariant(light: Color(0xff00BCD3), dark: Color(0xff00BCD3));
+  final othersTeal =
+      const CRColorVariant(light: Color(0xff009689), dark: Color(0xff009689));
+  final othersGreen =
+      const CRColorVariant(light: Color(0xff4AAF57), dark: Color(0xff4AAF57));
+  final othersLightGreen =
+      const CRColorVariant(light: Color(0xff8BC255), dark: Color(0xff8BC255));
+  final othersLime =
+      const CRColorVariant(light: Color(0xffCDDC4C), dark: Color(0xffCDDC4C));
+  final othersYellow =
+      const CRColorVariant(light: Color(0xffFFEB4F), dark: Color(0xffFFEB4F));
+  final othersAmber =
+      const CRColorVariant(light: Color(0xffFFC02D), dark: Color(0xffFFC02D));
+  final othersOrange =
+      const CRColorVariant(light: Color(0xffFF981F), dark: Color(0xffFF981F));
+  final othersDeepOrange =
+      const CRColorVariant(light: Color(0xffFF5726), dark: Color(0xffFF5726));
+  final othersBrown =
+      const CRColorVariant(light: Color(0xff7A5548), dark: Color(0xff7A5548));
+  final othersBlueGrey =
+      const CRColorVariant(light: Color(0xff607D8A), dark: Color(0xff607D8A));
+  final dark1 =
+      const CRColorVariant(light: Color(0xff181A20), dark: Color(0xff181A20));
+  final dark2 =
+      const CRColorVariant(light: Color(0xff1E2025), dark: Color(0xff1E2025));
+  final dark3 =
+      const CRColorVariant(light: Color(0xff1F222A), dark: Color(0xff1F222A));
+  final dark4 =
+      const CRColorVariant(light: Color(0xff262A35), dark: Color(0xff262A35));
+  final dark5 =
+      const CRColorVariant(light: Color(0xff35383F), dark: Color(0xff35383F));
+  final backgroundTeal =
+      const CRColorVariant(light: Color(0xffF0FBFA), dark: Color(0xffF0FBFA));
+  final backgroundPurple =
+      const CRColorVariant(light: Color(0xffF8F0FE), dark: Color(0xffF8F0FE));
+  final backgroundRed =
+      const CRColorVariant(light: Color(0xffFFF3F3), dark: Color(0xffFFF3F3));
+  final backgroundBlue =
+      const CRColorVariant(light: Color(0xffF3F6FF), dark: Color(0xffF3F6FF));
+  final backgroundGreen =
+      const CRColorVariant(light: Color(0xffEDFBF7), dark: Color(0xffEDFBF7));
+  final backgroundBrown =
+      const CRColorVariant(light: Color(0xffFBF6F3), dark: Color(0xffFBF6F3));
+  final backgroundYellow =
+      const CRColorVariant(light: Color(0xffFFFCEB), dark: Color(0xffFFFCEB));
+  final backgroundOrange =
+      const CRColorVariant(light: Color(0xffFFF8EE), dark: Color(0xffFFF8EE));
+  final transparentTeal = const CRColorVariant(
+      light: Color(0xff1A998E14), dark: Color(0xff1A998E14));
+  final transparentPurple = const CRColorVariant(
+      light: Color(0xff9610FF14), dark: Color(0xff9610FF14));
+  final transparentRed = const CRColorVariant(
+      light: Color(0xffFF4A4A14), dark: Color(0xffFF4A4A14));
+  final transparentBlue = const CRColorVariant(
+      light: Color(0xff235DFF14), dark: Color(0xff235DFF14));
+  final transparentGreen = const CRColorVariant(
+      light: Color(0xff17CE9214), dark: Color(0xff17CE9214));
+  final transparentBrown = const CRColorVariant(
+      light: Color(0xffA4634D14), dark: Color(0xffA4634D14));
+  final transparentYellow = const CRColorVariant(
+      light: Color(0xffFFD30014), dark: Color(0xffFFD30014));
+  final transparentOrange = const CRColorVariant(
+      light: Color(0xffF8930014), dark: Color(0xffF8930014));
+  final transparentGrey = const CRColorVariant(
+      light: Color(0xff75757514), dark: Color(0xff75757514));
+  final backgroundGrey =
+      const CRColorVariant(light: Color(0xffF6F6F6), dark: Color(0xffF6F6F6));
+}
